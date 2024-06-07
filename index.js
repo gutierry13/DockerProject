4e9a355353ed338ac389b60b7009660d4c4a4a94
Gutierry <aleguti1000@hotmail.com>
2024-06-07 12:10:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express"
+import mysql from "mysql2"
+const app = express();
+const port = 3000;
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'teste'
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+  console.log('Connected to database');
+});
+
+app.get('/consulta-dados', (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      res.status(500).send('Error fetching data');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
